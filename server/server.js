5f00ba7fc1ad8a347e8e5d65d2350d46b7cf74ea
SAMIR DHORAN <samirdhoran2000@gmail.com>
2024-10-22 22:25:04 +0530

    update cors

--- server/server.js
+++ server/server.js
@@ -27,7 +27,7 @@ mongoose
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors('*'));
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "http://13.202.214.198:5173");
