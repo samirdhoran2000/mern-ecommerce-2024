5727797e4c403406b94a5388d11870e233092f0f
SAMIR DHORAN <samirdhoran2000@gmail.com>
2024-10-22 22:19:49 +0530

    cors update

--- server/server.js
+++ server/server.js
@@ -29,6 +29,17 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://13.202.214.198:5173");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  next();
+});
+
 app.use(cookieParser());
 app.use(express.json());
 app.use("/api/auth", authRouter);
